d314a008bc9671a7ab2acd403aa9af3158c4886f
Yamandi Ruslan <ruslanjamandi13@gmail.com>
2024-02-26 23:10:19 +0100

    HW1:v2

--- HW1/memory_profiler.py
+++ HW1/memory_profiler.py
@@ -1,19 +1,20 @@
-import os
-import psutil
+import functools
+import tracemalloc
 
 
-def get_process_memory():
-    process = psutil.Process(os.getpid())
-    mem_info = process.memory_info()
-    return mem_info.rss
+def get_memory():
+    mem = tracemalloc.get_tracemalloc_memory()
+    return round(mem / 1024, 2)
 
 
 def profile(func):
+    @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        mem_before = get_process_memory()
+        tracemalloc.start()
+        mem_before = get_memory()
         result = func(*args, **kwargs)
-        mem_after = get_process_memory()
-        print("{}: memory before: {:,}, after: {:,}, consumed: {:,};".format(
+        mem_after = get_memory()
+        print("{}: memory before: {:,} KB, after: {:,} KB, consumed: {:,} KB;".format(
             func.__name__, mem_before, mem_after, mem_after - mem_before
         ))
         return result
