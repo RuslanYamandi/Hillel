b5e8d78884c2c37a82512545da164fb367017939
Yamandi Ruslan <ruslanjamandi13@gmail.com>
2024-03-03 20:33:02 +0100

    HW2:v3

--- HW2/password_generator.py
+++ HW2/password_generator.py
@@ -3,19 +3,15 @@ import random
 
 
 def generate_password():
-    s1 = list(string.ascii_lowercase)
-    s2 = list(string.ascii_uppercase)
-    s3 = list(string.digits)
-    s4 = list(string.punctuation)
-    chars = s1 + s2 + s3 + s4
-
+    chars = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
     password_length = random.randint(10, 16)
+
     password = [
-        random.choice(s1),
-        random.choice(s2),
-        random.choice(s3),
-        random.choice(s4),
-    ] + [random.choice(chars) for _ in range(password_length)]
+        random.choice(string.ascii_lowercase),
+        random.choice(string.ascii_uppercase),
+        random.choice(string.digits),
+        random.choice(string.punctuation),
+    ] + random.choices(chars, k=password_length)
 
     random.shuffle(password)
     return ''.join(password)
