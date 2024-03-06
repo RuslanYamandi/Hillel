33af8b18123f7858b9935afb3c215bc23f5d1fc6
Yamandi Ruslan <ruslanjamandi13@gmail.com>
2024-03-06 08:41:52 +0100

    HW3:v2

--- HW2/main.py
+++ HW2/main.py
@@ -11,11 +11,13 @@ from faker import Faker
 
 app = Flask(__name__)
 
+FILE_NAME = 'users.csv'
+
 
 @app.route('/')
 def hello_world():
     links = [
-        '<a href="/generate_password">Generate students</a>',
+        '<a href="/generate_students">Generate students</a>',
         '<a href="/get_bitcoin_value">Get Bitcoin value</a>'
     ]
 
@@ -36,19 +38,17 @@ def generate_students(count: int):
     fake = Faker()
     users = []
     for i in range(count):
-        user_name = fake.name().split(' ')
         user = {
-            'first_name': user_name[0],
-            'last_name': user_name[1],
+            'first_name': fake.first_name(),
+            'last_name': fake.last_name(),
             'email': fake.email(),
             'password': fake.password(length=8),
-            'age': random.randint(18, 60)
+            'birthday': fake.date_between(start_date='-60y', end_date='-18y').isoformat()
         }
         users.append(user)
 
-    file_name = 'users.csv'
-    with open(file_name, 'w', newline='') as csvfile:
-        headers = ['first_name', 'last_name', 'email', 'password', 'age']
+    with open(FILE_NAME, 'w', newline='') as csvfile:
+        headers = ['first_name', 'last_name', 'email', 'password', 'birthday']
         writer = csv.DictWriter(csvfile, fieldnames=headers)
         writer.writeheader()
         writer.writerows(users)
@@ -58,7 +58,7 @@ def generate_students(count: int):
 @app.route('/get_bitcoin_value')
 @use_kwargs(
     {
-        'currency': fields.Str(missing='USD', validate=validate.OneOf(['USD', 'EUR', 'UAH'])),
+        'currency': fields.Str(missing='USD'),
         'convert': fields.Int(missing=1, validate=validate.Range(
             min=1, min_inclusive=True
         )),
@@ -68,23 +68,27 @@ def generate_students(count: int):
 def get_bitcoin_value(currency: str, convert: int):
     response = requests.get('https://bitpay.com/api/rates')
     if response.status_code not in [HTTPStatus.OK]:
-        return Response('Error: something went wrong', status=response.status_code)
+        return Response('Error: something went wrong.', status=response.status_code)
 
     data: dict = response.json()
     result = ''
+    symbol = ''
+
     for entity in data:
         if entity.get('code') == currency:
             rate = entity.get('rate')
             result = str(rate * convert)
             break
 
-    currency_response = requests.get('https://test.bitpay.com/currencies')
-    currency_data: dict = currency_response.json()
-    symbol = ''
-    for currency_entity in currency_data.get('data'):
-        if currency_entity.get('code') == currency:
-            symbol = currency_entity.get('symbol')
-            break
+    if not result:
+        return Response('Error: currency provided is not correct.')
+    else:
+        currency_response = requests.get('https://test.bitpay.com/currencies')
+        currency_data: dict = currency_response.json()
+        for currency_entity in currency_data.get('data'):
+            if currency_entity.get('code') == currency:
+                symbol = currency_entity.get('symbol')
+                break
 
     return symbol + result
 
